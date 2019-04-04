a8857c7700c7cb0cc0daea9a4ceba61eed84ca59
alumno01 <alumno01@enpresadigitala.local>
2019-04-04 11:35:58 +0200

    Exercise 2

--- cypress/integration/primertest.spec.js
+++ cypress/integration/primertest.spec.js
@@ -36,4 +36,46 @@ describe("Test Irontec", () => {
       cy.get("body").should("have.class", "onpane2");
     });
   });
+
+  context("Form", () => {
+    before(() => {
+      cy.get(".headerOpenAwards2017 > a").invoke("attr", "target", "_self");
+      cy.get(".headerOpenAwards2017 > a").click();
+    });
+
+    it("Comprobar que el checkbox no esté seleccionado", () => {
+      cy.get("#privacityCheck").should("not.be.checked");
+    });
+
+    it("Rellenar el nombre, el apellido, seleccionar algo select y aceptar la política de privacidad, y enviar", () => {
+      cy.get(".form-empleo").within(() => {
+        cy.get("#name").type("Agapito");
+        cy.get("#lastName").type("DiSousa");
+        cy.get("#work").select("Internet es mi única religión");
+        cy.get("#privacityCheck").check();
+        cy.root().submit();
+      });
+    });
+
+    it("Comprobar el mensaje de error", () => {
+      cy.get("#alert-warning > strong").should(
+        "contain",
+        "Los campos con * son obligatorios"
+      );
+    });
+
+    it("Limpiar los campos nombre y apellido", () => {
+      cy.get("#name")
+        .clear()
+        .should("have.value", "");
+      cy.get("#lastName")
+        .clear()
+        .should("have.value", "");
+    });
+
+    it("Cambiar el viewport a móvil y testear que el menú móvil se muestra", () => {
+      cy.viewport("iphone-6");
+      cy.get(".btnSidebarMenu > .si-icons").should("exist");
+    });
+  });
 });
